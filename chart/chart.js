900a2858bd67c723fd019f2e4216e9c698fb5be3
Jaro Hokkanen <jaro.hokkanen@gmail.com>
2023-12-04 08:37:46 +0200

    Add kW plotting option

--- chart/chart.js
+++ chart/chart.js
@@ -7,34 +7,26 @@ import data_easee from 'url:../workspace/easee.csv';
 // Import st-entsoe data for the chart (can be commented out)
 import data_st from 'url:../workspace/st-entsoe.csv';
 
-// Aux function to get the beginning and end of the day
-function date_lims(start_date, end_date) {
-    let bod = new Date(start_date);
-    // Set the beginning of the current day
-    bod.setHours(0, 0, 0, 0);
-    let eod = new Date(end_date);
-    // Set the beginning of the next day
-    eod.setDate(eod.getDate() + 1);
-    // Add extra 1min to include the first point of the next day
-    eod.setHours(0, 1, 0, 0);
-    return { bod, eod };
-}
+// The local electric grid voltage for all phases
+const VOLTAGE = 230;
 
 // ChartDrawer class
 class ChartDrawer {
 
     // Chart vars
     #chart;
-    #max_time;
-    #min_time;
+    #max_time_unix;
+    #min_time_unix;
 
     // Dataset 1 (data_easee) vars
     #ch_curr1;
     #ch_curr2;
     #ch_curr3;
+    #ch_total;
     #eq_curr1;
     #eq_curr2;
     #eq_curr3;
+    #eq_total;
 
     // Dataset 2 (data_st) vars
     #price;
@@ -42,23 +34,25 @@ class ChartDrawer {
     #temp_in;
     #temp_out;
 
-    async #initialize_values() {
+    #initialize_chart() {
         // Destroy the chart if it already exists
         if (this.#chart)
             this.#chart.destroy();
 
         // Initialize all values
         this.#chart = null;
-        this.#max_time = -Infinity;
-        this.#min_time = Infinity;
+        this.#max_time_unix = -Infinity;
+        this.#min_time_unix = Infinity;
 
         // Dataset 2 (data_easee) vars
         this.#ch_curr1 = [];
         this.#ch_curr2 = [];
         this.#ch_curr3 = [];
+        this.#ch_total = [];
         this.#eq_curr1 = [];
         this.#eq_curr2 = [];
         this.#eq_curr3 = [];
+        this.#eq_total = [];
 
         // Dataset 2 (data_st) vars
         this.#price = [];
@@ -67,28 +61,38 @@ class ChartDrawer {
         this.#temp_out = [];
     }
 
+    // Get the beginning and end of the day
+    #date_lims(start_date, end_date) {
+        let bod_date = new Date(start_date);
+        // Set the beginning of the current day
+        bod_date.setHours(0, 0, 0, 0);
+        let eod_date = new Date(end_date);
+        // Set the beginning of the next day
+        eod_date.setDate(eod_date.getDate() + 1);
+        // Add extra 1min to include the first point of the next day
+        eod_date.setHours(0, 1, 0, 0);
+        // Return the Unix timestamps
+        const bod = Math.floor(new Date(bod_date.getTime()) / 1000);
+        const eod = Math.floor(new Date(eod_date.getTime()) / 1000);
+        return { bod, eod };
+    }
+
     // Setup the chart
     async #setup_chart() {
-
-        // Use the date_lims function to get the beginning and end of the day (remove the extra 60 secs from eod)
-        const time_limits = date_lims(new Date(this.#min_time * 1000), new Date((this.#max_time - 60) * 1000));
-
-        // Convert the time limits to Unix timestamps
-        const time_min_unix = Math.floor(time_limits.bod.getTime() / 1000);
-        const time_max_unix = Math.floor(time_limits.eod.getTime() / 1000);
-
-        // Create new chart
+        // Create a new chart
         const ctx = document.getElementById('acquisitions').getContext('2d');
         this.#chart = new Chart(ctx, {
             type: 'line',
             data: {
                 datasets: [
-                    { label: 'Charger 1', yAxisID: 'y_left', data: this.#ch_curr1, backgroundColor: 'rgba(0, 255, 255, 0.5)', borderColor: 'transparent', fill: 'origin', pointRadius: 0, stepped: 'middle' },
-                    { label: 'Charger 2', yAxisID: 'y_left', data: this.#ch_curr2, backgroundColor: 'rgba(255, 0, 255, 0.5)', borderColor: 'transparent', fill: 'origin', pointRadius: 0, stepped: 'middle' },
-                    { label: 'Charger 3', yAxisID: 'y_left', data: this.#ch_curr3, backgroundColor: 'rgba(255, 255, 0, 0.5)', borderColor: 'transparent', fill: 'origin', pointRadius: 0, stepped: 'middle' },
-                    { label: 'Equalizer 1', yAxisID: 'y_left', data: this.#eq_curr1, borderColor: 'cyan', borderWidth: 1, fill: false, pointRadius: 0, stepped: 'middle' },
-                    { label: 'Equalizer 2', yAxisID: 'y_left', data: this.#eq_curr2, borderColor: 'magenta', borderWidth: 1, fill: false, pointRadius: 0, stepped: 'middle' },
-                    { label: 'Equalizer 3', yAxisID: 'y_left', data: this.#eq_curr3, borderColor: 'yellow', borderWidth: 1, fill: false, pointRadius: 0, stepped: 'middle' },
+                    { label: 'Charger 1 (A)', yAxisID: 'y_left', data: this.#ch_curr1, backgroundColor: 'rgba(0, 255, 255, 0.5)', borderColor: 'transparent', fill: 'origin', pointRadius: 0, stepped: 'middle' },
+                    { label: 'Charger 2 (A)', yAxisID: 'y_left', data: this.#ch_curr2, backgroundColor: 'rgba(255, 0, 255, 0.5)', borderColor: 'transparent', fill: 'origin', pointRadius: 0, stepped: 'middle' },
+                    { label: 'Charger 3 (A)', yAxisID: 'y_left', data: this.#ch_curr3, backgroundColor: 'rgba(255, 255, 0, 0.5)', borderColor: 'transparent', fill: 'origin', pointRadius: 0, stepped: 'middle' },
+                    { label: 'Charger Total (kW)', yAxisID: 'y_left', data: this.#ch_total, backgroundColor: 'rgba(255, 0, 0, 0.5)', borderColor: 'transparent', fill: 'origin', pointRadius: 0, stepped: 'middle' },
+                    { label: 'Equalizer 1 (A)', yAxisID: 'y_left', data: this.#eq_curr1, borderColor: 'cyan', borderWidth: 1, fill: false, pointRadius: 0, stepped: 'middle' },
+                    { label: 'Equalizer 2 (A)', yAxisID: 'y_left', data: this.#eq_curr2, borderColor: 'magenta', borderWidth: 1, fill: false, pointRadius: 0, stepped: 'middle' },
+                    { label: 'Equalizer 3 (A)', yAxisID: 'y_left', data: this.#eq_curr3, borderColor: 'yellow', borderWidth: 1, fill: false, pointRadius: 0, stepped: 'middle' },
+                    { label: 'Equalizer Total (kW)', yAxisID: 'y_left', data: this.#eq_total, borderColor: 'rgba(255, 0, 0, 1)', borderWidth: 1, fill: false, pointRadius: 0, stepped: 'middle' },
                     { label: 'Price', yAxisID: 'y_right', data: this.#price, borderColor: 'black', borderDash: [1, 3], borderWidth: 1, fill: false, pointRadius: 1, stepped: 'before' },
                     { label: 'Temp In', yAxisID: 'y_right', data: this.#temp_in, borderColor: 'green', borderDash: [4, 4], borderWidth: 1, fill: false, pointRadius: 1, tension: 0.4 },
                     { label: 'Temp Out', yAxisID: 'y_right', data: this.#temp_out, borderColor: 'blue', borderDash: [4, 4], borderWidth: 1, fill: false, pointRadius: 1, tension: 0.4 },
@@ -111,8 +115,7 @@ class ChartDrawer {
                         callbacks: {
                             title: function (context) {
                                 // Convert the Unix timestamp to a Date object and format the date
-                                const date = new Date(context[0].parsed.x * 1000).toLocaleString('en-UK');
-                                return date;
+                                return new Date(context[0].parsed.x * 1000).toLocaleString('en-UK');
                             },
                         }
                     }
@@ -121,32 +124,30 @@ class ChartDrawer {
                     x: {
                         type: 'linear',
                         beginAtZero: false,
-                        min: time_min_unix,
-                        max: time_max_unix,
+                        min: this.#min_time_unix,
+                        max: this.#max_time_unix,
                         ticks: {
                             source: 'data',
                             autoSkip: true,
-                            stepSize: (time_max_unix - time_min_unix) / 24,
+                            stepSize: (this.#max_time_unix - this.#min_time_unix) / 24,
                             callback: function (value, index, values) {
-                                // Convert the Unix timestamp to a Date object
-                                const date = new Date(value * 1000);
-                                // Format the date
-                                return date.toLocaleString('en-UK');
+                                // Convert the Unix timestamp to a Date object and format the date
+                                return new Date(value * 1000).toLocaleString('en-UK');
                             }
                         }
                     },
                     y_left: {
                         beginAtZero: true,
                         grid: {
-                            color: 'rgba(0, 128, 128, 0.2)',
+                            color: 'rgba(255, 0, 0, 0.2)',
                         },
                         ticks: {
-                            color: 'rgba(0, 128, 128, 1)',
+                            color: 'rgba(255, 0, 0, 1)',
                         },
                         title: {
                             display: true,
-                            color: 'rgba(0, 128, 128, 1)',
-                            text: 'Electric Current (A)'
+                            color: 'rgba(255, 0, 0, 1)',
+                            text: 'Current (A) / Power (kW)'
                         }
                     },
                     y_right: {
@@ -164,6 +165,11 @@ class ChartDrawer {
     // Parse data using PapaParse
     async #parse_data(data, start_time_unix, end_time_unix, callback) {
         return new Promise((resolve, reject) => {
+            // Store the local min and max time values for the searched time period
+            let max_time = -Infinity;
+            let min_time = Infinity;
+
+            // Parse data
             Papa.parse(data, {
                 download: true,
                 header: true,
@@ -177,16 +183,25 @@ class ChartDrawer {
                         callback(row);
                         // Update min_time and max_time
                         if (row['unix_time'] !== null) {
-                            this.#min_time = Math.min(this.#min_time, row['unix_time']);
-                            this.#max_time = Math.max(this.#max_time, row['unix_time']);
+                            min_time = Math.min(min_time, row['unix_time']);
+                            max_time = Math.max(max_time, row['unix_time']);
                         }
                     }
                 },
                 complete: (results) => {
                     if (results.errors.length > 0)
                         reject(results.errors);
-                    else
+                    else {
+                        // Get the beginning and end of the day for min_time and max_time (remove the extra 60 secs from eod)
+                        const time_limits = this.#date_lims(new Date(min_time * 1000), new Date((max_time - 60) * 1000));
+
+                        // Convert the time limits to Unix timestamps and check if global min and max time values need updating
+                        this.#min_time_unix = Math.min(this.#min_time_unix, time_limits.bod);
+                        this.#max_time_unix = Math.max(this.#max_time_unix, time_limits.eod);
+
+                        // Resolve promise
                         resolve();
+                    }
                 },
                 error: (error) => {
                     reject(error);
@@ -227,14 +242,15 @@ class ChartDrawer {
     }
 
     // Generate the chart
-    async generateChart(start_date, end_date) {
+    async generate_chart(start_date, end_date) {
 
         // Destroy any existing charts and initialize all vars
-        await this.#initialize_values();
+        this.#initialize_chart();
 
         // Convert the start and end dates to Unix timestamps
-        const start_time_unix = new Date(start_date).getTime() / 1000;
-        const end_time_unix = new Date(end_date).getTime() / 1000;
+        const limits = this.#date_lims(start_date, end_date);
+        const start_time_unix = limits.bod;
+        const end_time_unix = limits.eod;
 
         // Parse data_easee and populate the dataset
         try {
@@ -242,9 +258,11 @@ class ChartDrawer {
                 this.#ch_curr1.push({ x: row['unix_time'], y: row['ch_curr1'] });
                 this.#ch_curr2.push({ x: row['unix_time'], y: row['ch_curr2'] });
                 this.#ch_curr3.push({ x: row['unix_time'], y: row['ch_curr3'] });
+                this.#ch_total.push({ x: row['unix_time'], y: VOLTAGE * (row['ch_curr1'] + row['ch_curr2'] + row['ch_curr3']) / 1000 });
                 this.#eq_curr1.push({ x: row['unix_time'], y: row['eq_curr1'] });
                 this.#eq_curr2.push({ x: row['unix_time'], y: row['eq_curr2'] });
                 this.#eq_curr3.push({ x: row['unix_time'], y: row['eq_curr3'] });
+                this.#eq_total.push({ x: row['unix_time'], y: VOLTAGE * (row['eq_curr1'] + row['eq_curr2'] + row['eq_curr3']) / 1000 });
             }).catch((error) => console.log(error));
         } catch (error) {
             console.log(error);
@@ -271,6 +289,44 @@ class ChartDrawer {
         // Update the chart to show heat_on correctly
         this.#chart.update();
     }
+
+    // Choose between individual phases and total power layouts
+    get_actions() {
+        return [
+            {
+                name: 'Individual phases (A)',
+                handler() {
+                    this.#chart.data.datasets[0].hidden = false;
+                    this.#chart.data.datasets[1].hidden = false;
+                    this.#chart.data.datasets[2].hidden = false;
+                    this.#chart.data.datasets[3].hidden = true;
+                    this.#chart.data.datasets[4].hidden = false;
+                    this.#chart.data.datasets[5].hidden = false;
+                    this.#chart.data.datasets[6].hidden = false;
+                    this.#chart.data.datasets[7].hidden = true;
+                    this.#chart.update();
+                }
+            },
+            {
+                name: 'Total power (kW)',
+                handler() {
+                    this.#chart.data.datasets[0].hidden = true;
+                    this.#chart.data.datasets[1].hidden = true;
+                    this.#chart.data.datasets[2].hidden = true;
+                    this.#chart.data.datasets[3].hidden = false;
+                    this.#chart.data.datasets[4].hidden = true;
+                    this.#chart.data.datasets[5].hidden = true;
+                    this.#chart.data.datasets[6].hidden = true
+                    this.#chart.data.datasets[7].hidden = false
+                    this.#chart.update();
+                }
+            },
+        ];
+    }
+    // Apply the action
+    apply_action(action) {
+        action.handler.call(this);
+    }
 }
 
 // Begin execution here
@@ -295,17 +351,24 @@ class ChartDrawer {
         let start_date = new Date(document.getElementById('dateInput').value);
         let end_date = document.getElementById('rangeCheckbox').checked
             ? new Date(document.getElementById('endDateInput').value) : new Date(start_date.getTime());
-        limits = date_lims(start_date, end_date);
-        chart_drawer.generateChart(limits.bod, limits.eod);
+        chart_drawer.generate_chart(start_date, end_date);
     });
 
     // Reset the chart data to the whole original data
     document.getElementById('showAllButton').addEventListener('click', function () {
-        limits = date_lims(new Date(0), new Date());
-        chart_drawer.generateChart(limits.bod, limits.eod);
+        chart_drawer.generate_chart(new Date(0), new Date());
+    });
+
+    // Switch between individual phases and total power layouts
+    const chartActions = document.getElementById('chartActions');
+    chart_drawer.get_actions().forEach(action => {
+        const button = document.createElement('button');
+        button.innerText = action.name;
+        button.addEventListener('click', () => chart_drawer.apply_action(action));
+        chartActions.appendChild(button);
     });
 
     // Generate the chart for the current day when the chart is first opened
-    limits = date_lims(new Date(), new Date());
-    chart_drawer.generateChart(limits.bod, limits.eod);
+    chart_drawer.generate_chart(new Date(), new Date());
+
 })();
